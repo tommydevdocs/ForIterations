c916bb98d2a4af7c66476b75d1ff610916b1c83e
tommydevdocs <tommydevdocs@gmail.com>
2015-06-17 16:53:00 -0500

    Committ Solution

---
+++ ForIterations/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        For index = 1 To 10
+            'Console.WriteLine(index)
+            If (index = 7) Then
+                Console.WriteLine("Found seven!")
+                Exit For
+            End If
+        Next
+        For i = 10 To 5 Step -1
+            Console.WriteLine(i)
+
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
